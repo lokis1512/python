30045ede0774c125635f951e2e55a9be6d75fc7b
loki <loki@gmail.com>
2021-05-05 08:59:43 +0530

    open

--- os_class.py
+++ os_class.py
@@ -1,9 +1,11 @@
 import os
+import subprocess
 a = os.getcwd()
 b = input("enter a file : ")
 c = os.path.join(a,b)
 if os.path.isfile(c):
-	print("ok")
+	print("file exist")
 else:
-	print("create a new file")
-	
+	subprocess.call(["touch",c])
+	print("file does not exist")
+
